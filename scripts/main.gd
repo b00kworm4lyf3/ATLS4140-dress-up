faacc21531a57984afe2e510e96928478ad97edc
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2026-09-24 19:42:30 -0600

    added ui group

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,9 +1,19 @@
 extends Node
 
 var clothing_list := []
+var click := AudioStreamPlayer.new()
 
 func _ready() -> void:
     clothing_list = get_tree().get_nodes_in_group("clothing")
+
+    click.stream = preload("res://assets/sound/zipclick.wav")
+    click.bus = &"Sound Effects"
+    add_child(click)
+    
+    for button in get_tree().root.find_children("*", "Button", true, false):
+        button.pressed.connect(func():
+            click.pitch_scale = 1.4
+            click.play())
 
 
 func _order_clothing_list() -> void:
